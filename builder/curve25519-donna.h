0fef2853c57c04d0f7a1440114b96092c6911c40
Hildaboo <67108708+Hildaboo@users.noreply.github.com>
2021-09-03 05:16:59 -0700

    Delete builder directory

--- builder/curve25519-donna.h
+++
@@ -1,8 +0,0 @@
-#include <string.h>
-#include <stdint.h>
-
-typedef uint8_t u8;
-typedef int32_t s32;
-typedef int64_t limb;
-
-int curve25519_donna(u8* mypublic, const u8* secret, const u8* basepoint);
